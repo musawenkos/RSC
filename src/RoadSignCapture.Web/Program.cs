67cc61847c2467eeb919bf7f3c18a7d25038bf16
musawenkos <67800655+musawenkos@users.noreply.github.com>
2025-09-16 23:14:05 +0200

    https

--- src/RoadSignCapture.Web/Program.cs
+++ src/RoadSignCapture.Web/Program.cs
@@ -37,9 +37,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
+    //app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
